87a535e82b67b0054f501199e95bceb064a9bbcc
Suri <suri@uchile.cl>
2018-07-20 16:49:21 -0400

    Plotting real-time and storing in csv

--- plotter.py
+++ plotter.py
@@ -13,6 +13,7 @@ from datagetter import DataGetSerial
 import matplotlib.pyplot as plt
 import numpy as np
 from drawnow import *
+import csv
 
 def init_list_of_objects(size):
     list_of_objects = list()
@@ -27,7 +28,6 @@ y = init_list_of_objects(n)
 t = []
 dt = 0.01
 cnt = 0
-
 def MakeFig():
     plt.ylim(np.amin(y)-2, np.amax(y)+2)
     plt.title("Grafico en tiempo real de salida Arduino")
@@ -36,29 +36,35 @@ def MakeFig():
     plt.plot(t, y[0], 'r', t, y[1], 'g', t, y[2], 'b')#, label="Velocidad angular")
     #plt.legend(loc='upper left')
 
-while True:
-    if cnt%2 == 1:
+with open("data.csv", 'w', newline='') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    csvwriter.writerow(['ax', 'ay', 'az'])
+    while True:
+        reading = dgs.get_next()
+        print("THIS IS", reading)
+        if len(reading) == n+2:
+            line = []
+            for i in range(n):
+                try:
+                    curr = float(reading[i+1])
+                    y[i].append(curr)
+                    line.append(curr)
+                except ValueError:
+                    print("Value error!")
+                    y[i].append(0)
+                    line.append(0)
+            csvwriter.writerow(line)
+        else:
+            continue
+            #for i in range(n):
+            #    y[i].append(0)
+        if len(t) == 0: t.append(0)
+        else: t.append(t[-1] + dt)
+        if cnt%6 == 0:
+            drawnow(MakeFig)
+            plt.pause(.000001)
+        if cnt > 100:
+            t.pop(0)
+            for i in range(n):
+                y[i].pop(0)
         cnt += 1
-        pass
-    reading = dgs.get_next()
-    print("THIS IS", reading)
-    if len(reading) >= n+1:
-        for i in range(n):
-            try:
-                y[i].append(float(reading[i+1]))
-            except ValueError:
-                print("Value error!")
-                y[i].append(0)
-    else:
-        for i in range(n):
-            y[i].append(0)
-    if len(t) == 0: t.append(0)
-    else: t.append(t[-1] + dt)
-    drawnow(MakeFig)
-    plt.pause(.000001)
-    if cnt > 100:
-        t.pop(0)
-        for i in range(n):
-            y[i].pop(0)
-    else: cnt += 1
-    
